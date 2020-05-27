e8037c1b5c99d0a66e4bbf8d83fed8ce519381fd
vo le phuong thao <thaomup@MacBook-Air-cua-vo.local>
2020-05-28 00:42:00 +0900

    Updates

---
+++ precache-manifest.cbb770387a684afd8c6f7c6071b32b97.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa6f5f3fafe530ba548ea75032d82578",
+    "url": "/dictionary/index.html"
+  },
+  {
+    "revision": "5b2b23a95ca9f4d5c0c8",
+    "url": "/dictionary/static/css/main.a880d9e1.chunk.css"
+  },
+  {
+    "revision": "feb334962f4c7a04ff1d",
+    "url": "/dictionary/static/js/2.b739ffb1.chunk.js"
+  },
+  {
+    "revision": "5b2b23a95ca9f4d5c0c8",
+    "url": "/dictionary/static/js/main.f0eab89f.chunk.js"
+  },
+  {
+    "revision": "67d3f92be30e1c42b517",
+    "url": "/dictionary/static/js/runtime~main.dbf1fe99.js"
+  }
+]);
